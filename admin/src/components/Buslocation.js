9153322521f3c8a295b70fdb0d6d7e09047c0336
rishijain07 <78855205+rishijain07@users.noreply.github.com>
2024-04-06 02:18:27 +0530

    changes made in validity feature

--- admin/src/components/Buslocation.js
+++ admin/src/components/Buslocation.js
@@ -3,7 +3,7 @@ import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reac
 import { toast } from 'react-toastify';
 import config from '../config';
 import jsQR from 'jsqr'; // Import jsQR library
-import {getReservation} from '../Services'
+import { getReservation } from '../Services';
 
 class BusLocations extends Component {
     constructor(props) {
@@ -14,10 +14,9 @@ class BusLocations extends Component {
             location: '',
             routes: [],
             uniqueRouteNames: [],
-        
             qrData: null,
-            result:null, // State to store QR code data
-            // State to store API call result
+            fromValidityClicked: false,
+            toValidityClicked: false,
         };
     }
 
@@ -68,37 +67,43 @@ class BusLocations extends Component {
                 const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
                 const code = jsQR(imageData.data, imageData.width, imageData.height);
                 if (code) {
-                    let qrdataurl = code.data
-                    qrdataurl= qrdataurl.replace('http://localhost:3000/ticket/', '');
+                    let qrdataurl = code.data;
+                    qrdataurl = qrdataurl.replace('http://localhost:3000/ticket/', '');
                     this.setState({ qrData: qrdataurl });
                     console.log('QR Code Data:', qrdataurl);
-                    // Simulate API call with the QR code data
-                    getReservation(this.state.qrData).then(res => {
-                        // res = res.json()
-                        // this.setState({data: res})
-                        let hi = res.from
-
-                        if(this.state.selectedLocation == ''){
-                            toast.error('Select location')
-                        }else{
-                            if(hi == this.state.selectedLocation){
-                                toast.success("Valid Ticket")
-                            }else{
-                                toast.error("Invalid ticket")
+                    getReservation(this.state.qrData)
+                        .then((res) => {
+                            console.log(res)
+                            if(this.state.fromValidityClicked ){
+                                let hi = res.from;
+                                if (this.state.selectedLocation === '') {
+                                    toast.error('Select location');
+                                } else if (hi === this.state.selectedLocation) {
+                                    toast.success('Valid Ticket');
+                                } else {
+                                    toast.error('Invalid ticket');
+                                }
+                            }else if(this.state.toValidityClicked){
+                                let hi = res.to;
+                                if (this.state.selectedLocation === '') {
+                                    toast.error('Select location');
+                                } else if (hi === this.state.selectedLocation) {
+                                    toast.success('Valid Ticket');
+                                } else {
+                                    toast.error('Invalid ticket');
+                                }
+                            }else {
+                                toast.error('404 error')
                             }
-                        }
-                        console.log(hi)
-                    }).catch(err => {
-                        console.log(err)
-                    })
-                    // this.simulateAPICall();
+                            // console.log(hi);
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
                 } else {
                     console.log('No QR code found.');
-                    toast.error('No QR code found.')
+                    toast.error('No QR code found.');
                 }
-                
-
-                
             };
         };
 
@@ -106,43 +111,84 @@ class BusLocations extends Component {
         reader.readAsDataURL(file);
     };
 
-  
-    
+    handleFromValidityClick = () => {
+        this.setState({ fromValidityClicked: true, toValidityClicked: false, selectedLocation: '' });
+    };
+
+    handleToValidityClick = () => {
+        this.setState({ fromValidityClicked: false, toValidityClicked: true, selectedLocation: '' });
+    };
 
     render() {
-        const { selectedLocation, uniqueRouteNames, qrData, apiResult } = this.state;
+        const { selectedLocation, uniqueRouteNames, fromValidityClicked, toValidityClicked } = this.state;
         return (
             <Container style={{ width: '80%', marginTop: '1%', marginBottom: '1%' }}>
                 <Row>
                     <Col sm={12}>
-                        <h4>Set Bus Location</h4>
-                        <Form onSubmit={this.handleSubmit}>
-                            <FormGroup>
-                                <Label for="selectRoute">Select Location</Label>
-                                <Input
-                                    type="select"
-                                    name="selectedLocation"
-                                    id="selectRoute"
-                                    value={selectedLocation}
-                                    onChange={this.handleChange}
-                                >
-                                    <option value="">Select a Location</option>
-                                    {uniqueRouteNames.map((routeName, index) => (
-                                        <option key={index} value={routeName}>
-                                            {routeName}
-                                        </option>
-                                    ))}
-                                </Input>
-                            </FormGroup>
-                        </Form>
+                        <Button onClick={this.handleFromValidityClick} color="primary" className="mr-3">
+                            From Validity
+                        </Button>
+                        <Button onClick={this.handleToValidityClick} color="primary">
+                            To Validity
+                        </Button>
                     </Col>
                 </Row>
+                {fromValidityClicked && (
+                    <Row>
+                        <Col sm={12}>
+                            <h4>Select From Location</h4>
+                            <Form>
+                                <FormGroup>
+                                    <Label for="selectLocation">Select Location</Label>
+                                    <Input
+                                        type="select"
+                                        name="selectedLocation"
+                                        id="selectLocation"
+                                        value={selectedLocation}
+                                        onChange={this.handleChange}
+                                    >
+                                        <option value="">Select a Location</option>
+                                        {uniqueRouteNames.map((routeName, index) => (
+                                            <option key={index} value={routeName}>
+                                                {routeName}
+                                            </option>
+                                        ))}
+                                    </Input>
+                                </FormGroup>
+                            </Form>
+                        </Col>
+                    </Row>
+                )}
+                {toValidityClicked && (
+                    <Row>
+                        <Col sm={12}>
+                            <h4>Select To Location</h4>
+                            <Form>
+                                <FormGroup>
+                                    <Label for="selectLocation">Select Location</Label>
+                                    <Input
+                                        type="select"
+                                        name="selectedLocation"
+                                        id="selectLocation"
+                                        value={selectedLocation}
+                                        onChange={this.handleChange}
+                                    >
+                                        <option value="">Select a Location</option>
+                                        {uniqueRouteNames.map((routeName, index) => (
+                                            <option key={index} value={routeName}>
+                                                {routeName}
+                                            </option>
+                                        ))}
+                                    </Input>
+                                </FormGroup>
+                            </Form>
+                        </Col>
+                    </Row>
+                )}
                 <Row>
                     <Col sm={12} className="text-center">
                         <h4 className="mt-5">Scan Your Ticket Here</h4>
                         <input type="file" accept="image/png" onChange={this.handleFileUpload} />
-                        
-                       
                     </Col>
                 </Row>
             </Container>

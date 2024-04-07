6fd90df064c341ec75b8d0baa83892de900d189d
rishijain07 <78855205+rishijain07@users.noreply.github.com>
2024-04-07 15:56:19 +0530

    changes done in contact form

--- services/routers/contact.js
+++ services/routers/contact.js
@@ -10,7 +10,7 @@ router.post('/railway/contact', async (req, res) => {
         var result = await contact.save()
         const phone = body.phone ? 'Phone :<b> ' + body.phone + ' </b><br> ' : ''
         const html = '<h2><u>User Contact</u></h2><p>Reference No : <b> ' + result._id + ' </b><br><br>Name : <b> ' + body.fname + ' ' + body.lname + ' </b><br> ' + phone + '  Email :<b> ' + body.email + ' </b><br>Message : <b>' + body.message + ' </b></p> '
-        client.sendEmail({ ...body, html: html, subject: 'User Contact', email: body.email + ', sl.railway.e.ticketing@gmail.com' })
+        client.sendEmail({ ...body, html: html, subject: 'User Contact', email: body.email + ', busticket40@gmail.com' })
         res.status(200).json(result)
     } catch (err) {
         res.status(500).json(err)
